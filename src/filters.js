0f937d0e88c8565cce662f17fb250bf908ed9716
Jesus Rogelio Garza <jesus.r.g27@gmail.com>
2019-11-15 00:12:06 -0600

    Added .gitignore file and CNAME for application url hosting by surge

--- src/filters.js
+++ src/filters.js
@@ -1,32 +1,32 @@
 // Set up filters default object
 const filters = {
-    searchText: '',
-    hideCompleted: false
-}
+  searchText: "",
+  hideCompleted: false
+};
 
 // getFilters
 // Arguments: none
 // Return value: filters object
-const getFilters = () => filters
+const getFilters = () => filters;
 
 // setFilters
 // Arguments: updates object with optional searchText or hideCompleted
 // Return value: none
 const setFilters = ({ searchText, hideCompleted }) => {
-    const st = typeof searchText === 'string'
-    const hc = typeof hideCompleted === 'boolean'
+  const st = typeof searchText === "string";
+  const hc = typeof hideCompleted === "boolean";
 
-    if(st && hc) {
-        filters.searchText = searchText
-        filters.hideCompleted = hideCompleted
-    } else if(st) {
-        filters.searchText = searchText
-    } else if(hc) {
-        filters.hideCompleted = hideCompleted
-    } else {
-        console.log('filter property not set to valid datatype')
-    }
-}
+  if (st && hc) {
+    filters.searchText = searchText;
+    filters.hideCompleted = hideCompleted;
+  } else if (st) {
+    filters.searchText = searchText;
+  } else if (hc) {
+    filters.hideCompleted = hideCompleted;
+  } else {
+    console.log("filter property not set to valid datatype");
+  }
+};
 
 // Make sure to set up the exports
-export { getFilters, setFilters }
+export { getFilters, setFilters };
--- src/index.js
+++ src/index.js
@@ -2,49 +2,49 @@
 // Make sure to load uuid via an npm module when necessary
 
 // --
-const searchText = document.querySelector('#search-text')
-const checkBox = document.querySelector('#hide-completed')
-const newTodo = document.querySelector('#new-todo')
+const searchText = document.querySelector("#search-text");
+const checkBox = document.querySelector("#hide-completed");
+const newTodo = document.querySelector("#new-todo");
 
-import renderTodos from './views'
-import { createTodo, loadTodos } from './todos'
-import { setFilters } from './filters';
+import renderTodos from "./views";
+import { createTodo, loadTodos } from "./todos";
+import { setFilters } from "./filters";
 
 // Render initial todos
-renderTodos()
+renderTodos();
 
 // Set up search text handler
-searchText.addEventListener('input', (e) => {
-    setFilters({
-        searchText: e.target.value
-    })
-    renderTodos()
-})
+searchText.addEventListener("input", e => {
+  setFilters({
+    searchText: e.target.value
+  });
+  renderTodos();
+});
 
 // Set up checkbox handler
-checkBox.addEventListener('change', (e) => {
-    setFilters({
-        hideCompleted: e.target.checked
-    })
-    renderTodos()
-})
+checkBox.addEventListener("change", e => {
+  setFilters({
+    hideCompleted: e.target.checked
+  });
+  renderTodos();
+});
 
 // Set up form submission handler
-newTodo.addEventListener('submit', (e) => {
-    const text = e.target.elements.text.value.trim() // omits leading and following spaces
-    e.preventDefault()
-    if (text.length > 0) {
-        createTodo(text)
-        renderTodos()
-        e.target.elements.text.value = ''
-        document.getElementById('focus').focus()
-    }
-})
+newTodo.addEventListener("submit", e => {
+  const text = e.target.elements.text.value.trim(); // omits leading and following spaces
+  e.preventDefault();
+  if (text.length > 0) {
+    createTodo(text);
+    renderTodos();
+    e.target.elements.text.value = "";
+    document.getElementById("focus").focus();
+  }
+});
 
 // Bonus: Add a watcher for local storage
-window.addEventListener('storage', (e) => {
-    if(e.key === 'todos') {
-        loadTodos()
-        renderTodos()
-    }
-})
+window.addEventListener("storage", e => {
+  if (e.key === "todos") {
+    loadTodos();
+    renderTodos();
+  }
+});
